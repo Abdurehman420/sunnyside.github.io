156b180b74fee1fbe74ca550b4e934be45633569
Abdurehman420 <94058932+Abdurehman420@users.noreply.github.com>
2022-03-10 21:46:22 +0500

    sunnyside website

---
+++ index.js
@@ -0,0 +1,37 @@
+const opend = document.querySelector('.openbar');
+const closed = document.querySelector('.sidebar__btn');
+const sidebar = document.querySelector('.sidebar');
+const change = document.getElementById('open');
+
+
+
+opend.addEventListener('click' , () =>{
+
+    sidebar.classList.add('open');
+})
+closed.addEventListener('click' , () =>{
+
+    sidebar.classList.remove('open');
+})
+
+
+
+window.addEventListener('scroll' , () =>{
+    var top = window.scrollY;
+
+    if (top > 1400 && top < 2100){
+        change.classList.add('show');
+
+    }
+    else if(top > 2900 && top < 3500){
+        change.classList.add('show');
+
+    }
+    else if (top > 5200 && top < 5500){
+        change.classList.add('show');
+
+    }
+    else{
+        change.classList.remove('show')
+    }
+})
